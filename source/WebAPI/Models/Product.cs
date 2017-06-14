4d2b294d876dc84ea6769846bd91f5a35e067fee
Igor Santos <igors.d@hotmail.com>
2017-06-14 01:00:56 -0300

    add models e controllers

---
+++ source/WebAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace WebAPI.Models
+{
+    public class Product
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public Product(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
