2ae0e03a0b0609eb079a9e6791f4a027b3169c91
Igor Santos <igors.d@hotmail.com>
2017-07-06 02:29:02 -0300

    ajuste de diretorios

---
+++ WebAPI/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WebAPI.Models
+{
+    public class ProductModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ WebAPI/Services/OAuthProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebAPI.Services
+{
+    public class OAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            
+            //TODO: Aqui Lógica para validar usuário e senha
+            //TODO: Verificar no if se usuário não está valido
+            if ()
+            {
+                context.SetError("invalid_grant", "Usuário não encontrado ou senha incorreta.");
+                return;
+            }
+
+            //emiti o token
+            var userIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+            context.Validated(userIdentity);
+        }
+    }
+}
